7a7cf4a7ffdd157f608105acaddb6468042a37ee
jp <jomz.liquido@gmail.com>
2022-04-10 17:33:23 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  state = {
+    books:[]
+  };
+
+  componentDidMount(){
+    this.getBooks();
+  }  
+
+  getBooks(){
+    axios
+    .get('http://127.0.0.1:8000/api/')
+    .then(res=>{
+      this.setState({books:res.data});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.books.map(item =>(
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.subtitle}</p>
+            <p>{item.author}</p>
+            <p>{item.isbn}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
